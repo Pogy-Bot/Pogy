4209494ee4b8a35542c34f7d8f496acbf90c58bc
Boeing_737 <nathaniel2007w@gmail.com>
2022-09-23 14:27:45 -0600

    Comments added

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 require("dotenv").config();
-const PogyClient = require("./Pogy");
+const PogyClient = require("./Pogy"); // Required for initial bot connection
 const config = require("./config.json");
 const logger = require("./src/utils/logger");
-const Pogy = new PogyClient(config);
+const Pogy = new PogyClient(config); // Required for client login
 
-const color = require("./src/data/colors");
+const color = require("./src/data/colors"); // colors
 Pogy.color = color;
 
-const emoji = require("./src/data/emoji");
+const emoji = require("./src/data/emoji"); // emojis (make sure to add your custom emojis to this file)
 Pogy.emoji = emoji;
 
 let client = Pogy;
-const jointocreate = require("./src/structures/jointocreate");
+const jointocreate = require("./src/structures/jointocreate"); // file required to make jointocreate
 jointocreate(client);
 
 Pogy.react = new Map();
 Pogy.fetchforguild = new Map();
 
-Pogy.start(process.env.TOKEN);
+Pogy.start(process.env.TOKEN); // NEEDED FOR BOT LOGIN, SHOULD NEVER BE REMOVED OR EDITED
 
 process.on("unhandledRejection", (reason, p) => {
   logger.info(`[unhandledRejection] ${reason.message}`, { label: "ERROR" });
