7596d9061c8c872b3c35bb960a70b6396d7e4824
Boeing 737 <101909226+Boeing-737-800@users.noreply.github.com>
2022-10-10 11:39:49 -0600

    Bug Fixes and Improvements

--- src/commands/moderation/modnick.js
+++ src/commands/moderation/modnick.js
@@ -46,6 +46,14 @@ module.exports = class extends Command {
       .setColor("RED")
       .setDescription(`${client.emoji.fail} | Please mention a valid member!`)
       return message.channel.sendCustom({ embeds: [validmention] })
+      .then(async (s) => {
+        if (logging && logging.moderation.delete_reply === "true") {
+          setTimeout(() => {
+            s.delete().catch(() => {});
+          }, 5000);
+        }
+      })
+      .catch(() => {});
     }
 
       if(!member) {
@@ -53,6 +61,29 @@ module.exports = class extends Command {
         .setColor("RED")
         .setDescription(`${client.emoji.fail} | I can't find that member`)
         return message.channel.sendCustom({ embeds: [usernotfound] })
+        .then(async (s) => {
+        if (logging && logging.moderation.delete_reply === "true") {
+          setTimeout(() => {
+            s.delete().catch(() => {});
+          }, 5000);
+        }
+      })
+      .catch(() => {});
+    }
+    
+    if(member.id === message.author.id) {
+      let modnickerror = new MessageEmbed()
+      .setColor("RED")
+      .setDescription(`${client.emoji.fail} | You can't moderate your own nickname!`)
+      return message.channel.sendCustom({ embeds: [modnickerror] })
+      .then(async (s) => {
+        if (logging && logging.moderation.delete_reply === "true") {
+          setTimeout(() => {
+            s.delete().catch(() => {});
+          }, 5000);
+        }
+      })
+      .catch(() => {});
     }
 
     if(member.roles.highest.position >= message.member.roles.highest.position) {
@@ -60,13 +91,14 @@ module.exports = class extends Command {
       .setColor("RED")
       .setDescription(`${client.emoji.fail} | They have more power than you or have equal power as you do!`)
       return message.channel.sendCustom({ embeds: [rolesmatch] })
-    }
-
-    if(member.id === message.author.id) {
-      let modnickerror = new MessageEmbed()
-      .setColor("RED")
-      .setDescription(`${client.emoji.fail} | You can't moderate your own nickname!`)
-      return message.channel.sendCustom({ embeds: [modnickerror] })
+      .then(async (s) => {
+        if (logging && logging.moderation.delete_reply === "true") {
+          setTimeout(() => {
+            s.delete().catch(() => {});
+          }, 5000);
+        }
+      })
+      .catch(() => {});
     }
 
     if(member) {
@@ -77,6 +109,22 @@ module.exports = class extends Command {
         .setDescription(`${client.emoji.success} | Moderated <@${member.id}>'s nickname for \`${reason || "No Reason Provided"}\``)
 
         message.channel.sendCustom({ embeds: [embed] })
+        .then(async (s) => {
+        if (logging && logging.moderation.delete_reply === "true") {
+          setTimeout(() => {
+            s.delete().catch(() => {});
+          }, 5000);
+        }
+      })
+      .catch(() => {});
+      }
+    if(member) {
+      let dmEmbed = new MessageEmbed()
+      .setColor("RED")
+      .setDescription(`**Nickname Moderated**\nYour nickname was moderated in **${message.guild.name}**. If you would like to change your nickname to something else, please reach out to a staff member.\n**Possible Reasons**\n• Your name was not typeable on a standard English QWERTY keyboard.\n• Your name contained words that are not suitable for the server.\n• Your name was not mentionable.\n\n__**Moderator:**__ ${message.author} **(${message.author.tag})**\n__**Reason:**__ ${reason || "No Reason Provided"}`)
+      .setTimestamp()
+
+      member.send({ embeds: [dmEmbed] })
     } else {
       return message.reply(`I can't moderate their nickname, make sure that my role is above theirs`)
     }
