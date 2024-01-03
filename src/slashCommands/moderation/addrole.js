61dfde2fcb1e3afb2319caceaa76f17e13d3c02a
Replit user <>
2024-01-03 04:28:40 +0000

    a

--- src/slashCommands/moderation/addrole.js
+++ src/slashCommands/moderation/addrole.js
@@ -6,8 +6,11 @@ module.exports = {
   data: new SlashCommandBuilder()
   .setName("addrole")
   .setDescription("Adds a role to a user.")
-  .addSubCommand((subcommand) => subcommand.setName("all").setDescription("Adds a role to a user.")
-    .addRoleOption((option) => option.setName("role").setDescription("The role to add to the user.").setRequired(true))
+  .addSubCommand((subcommand) => subcommand.setName("all").setDescription("Adds a role to all users.")
+    .addRoleOption((option) => option.setName("role").setDescription("The role to add to the users.").setRequired(true))
+    .addBooleanOption((option) => option.setName("remove").setDescription("Remove role or not")))
+  .addSubCommand((subcommand) => subcommand.setName("bots").setDescription("Adds a role to all bots.")
+    .addRoleOption((option) => option.setName("role").setDescription("The role to add to the bots.").setRequired(true))
     .addBooleanOption((option) => option.setName("remove").setDescription("Remove role or not"))),
   async execute(interaction) {
     if(!interaction.member.permissions.has("MANAGE_ROLES")) {
@@ -85,6 +88,76 @@ module.exports = {
             .catch(() => {})
           });
         };
+      }
+    } else if (interaction.options.getSubcommand() === "bots") {
+      const role = interaction.options.getRole("role") || interaction.guild.roles.cache.get(role) || interaction.guild.roles.cache.find((rl) => rl.name.toLowerCase() === role.slice(1).join(" ").toLowerCase());
+      const removerole = interaction.options.getBoolean("remove") || false;
+
+      let reason = `The current feature doesn't need a reason.`;
+      if (!reason) {
+        reason = `No Reason Provided`
+      }
+      if(reason.length > 1024) {
+        reason = reason.slice(0, 1021) + "...";
+      }
+
+      if(!role) {
+        let rolenotfound = new MessageEmbed()
+        .setAuthor({
+          name: `${interaction.user.tag}`,
+          iconURL: interaction.user.displayAvatarURL({ dynamic: true })
+        })
+        .setDescription(`${fail} | Please provide a valid role!`)
+        .setTimestamp()
+        .setFooter({ text: "https://mee8.ml" })
+        .setColor(interaction.client.color.red)
+        return interaction.reply({ embeds: [rolenotfound], ephmeral: true })
+        .then(async () => {
+          if (logging && logging.moderation.delete_reply === "true") {
+            setTimeout(() => {
+              interaction.deleteReply().catch(() => {});
+            }, 5000)
+          }
+        })
+        .catch(() => {});
+      } else {
+        if(removerole === false) {
+          interaction.channel.send({ content: `${success} | Adding ${role.name} to ${interaction.guild.members.cache.filter((m) => m.user.bot).size} bots. This may take a while!` }).then(() => {
+            interaction.guild.members.cache.filter((m) => m.user.bot).forEach((member) => member.roles.add(role, [
+              `Role Add / Responsible User: ${interaction.user.tag}`,
+            ]));
+            const embed = new MessageEmbed()
+            .setDescription(`${success} | Added **${role.name}** to **${interaction.guild.members.cache.filter((m) => m.user.bot).size}** bots.`)
+            .setColor(interaction.client.color.green)
+            interaction.channel.send({ embeds: [embed] })
+            .then(async () => {
+              if (logging && logging.moderation.delete_reply === "true") {
+                setTimeout(() => {
+                  interaction.deleteReply().catch(() => {});
+                }, 5000)
+              }
+            })
+            .catch(() => {})
+          });
+        } else {
+          interaction.channel.send({ content: `${success} | Removing ${role.name} from ${interaction.guild.members.cache.filter((m) => m.user.bot).size} bots. This may take a while!` }).then(() => {
+            interaction.guild.members.cache.filter((m) => m.user.bot).forEach((member) => member.roles.remove(role, [
+              `Role Remove / Responsible User: ${interaction.user.tag}`,
+            ]));
+            const embed = new MessageEmbed()
+            .setDescription(`${success} | Removed **${role.name}** from **${interaction.guild.members.cache.filter((m) => m.user.bot).size}** bots.`)
+            .setColor(interaction.client.color.green)
+            interaction.channel.send({ embeds: [embed] })
+            .then(async () => {
+              if (logging && logging.moderation.delete_reply === "true") {
+                setTimeout(() => {
+                  interaction.deleteReply().catch(() => {});
+                }, 5000)
+              }
+            })
+            .catch(() => {})
+          });
+        }
       }
     }
   }
