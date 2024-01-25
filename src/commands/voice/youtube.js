b33a47bbb522734e40f27789897a6a1cb6d58f74
hotsu0p <jwstephenson1000@gmail.com>
2024-01-25 14:00:38 -0600

    lil fix

--- src/commands/voice/youtube.js
+++ src/commands/voice/youtube.js
@@ -1,18 +1,24 @@
 const Command = require("../../structures/Command");
-const { createAudioPlayer, joinVoiceChannel, createAudioResource, StreamType } = require('@discordjs/voice');
-const { createReadStream } = require('fs');
-const path = require('path');
-const ytdl = require('ytdl-core');
-const { MessageEmbed } = require('discord.js');
+const {
+  createAudioPlayer,
+  joinVoiceChannel,
+  createAudioResource,
+  StreamType,
+} = require("@discordjs/voice");
+const { createReadStream } = require("fs");
+const path = require("path");
+const ytdl = require("ytdl-core");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = class EmptyCommand extends Command {
   constructor(...args) {
     super(...args, {
-      name: "empty",
+      name: "play",
       aliases: [],
-      description: "Empty command template.",
+      description: "play a song",
       category: "General",
-      cooldown: 5,
+      usage: "play <url>",
+      cooldown: 2,
     });
   }
 
@@ -21,7 +27,7 @@ module.exports = class EmptyCommand extends Command {
       if (!message.member.voice.channel) {
         return message.channel.send("You're not in a voice channel!");
       }
-     
+
       const connection = joinVoiceChannel({
         channelId: message.member.voice.channel.id,
         guildId: message.guild.id,
@@ -30,30 +36,27 @@ module.exports = class EmptyCommand extends Command {
 
       const player = createAudioPlayer();
 
-      const url = message.content.split(" ")[1]; // Assume the URL is the first argument after the command
+      const url = message.content.split(" ")[1];
 
       if (!url || !ytdl.validateURL(url)) {
         return message.channel.send("Invalid YouTube URL!");
       }
 
-      const info = await ytdl.getInfo(url); // Fetch video information
-
-      const stream = await ytdl(url, { filter: 'audioonly' });
-      const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
+      const info = await ytdl.getInfo(url);
+      const stream = await ytdl(url, { filter: "audioonly" });
+      const resource = createAudioResource(stream, {
+        inputType: StreamType.Arbitrary,
+      });
 
       player.play(resource);
 
       connection.subscribe(player);
 
-      // Truncate or limit the length of field value
-
-      // Create and send an embed with video information
       const embed = new MessageEmbed()
         .setTitle("Requested by " + message.author.username)
         .setDescription(`Playing ${url}`)
         .setColor("#FF5733");
       message.channel.send({ embeds: [embed] });
-    
     } catch (error) {
       console.error("Error playing audio:", error);
       message.channel.send("An error occurred during playback.");
