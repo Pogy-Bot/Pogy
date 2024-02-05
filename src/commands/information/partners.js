72aaded93abd08939d937b46b870b582e32f0d00
hotsu0p <jwstephenson1000@gmail.com>
2024-02-05 14:01:31 -0600

    Commited for parnters. @skillzl

--- src/commands/information/partners.js
+++ src/commands/information/partners.js
@@ -16,7 +16,7 @@ module.exports = class EmptyCommand extends Command {
   async run(message) {
     try {
      
-      message.channel.send("eres.fun");
+      message.channel.send("A multipurpose discord client written in javascript featuring a lot of util commands and a highly optimized web application with control over the application's settings. Eres is used on over 30 servers, we invite you to try it out and hope you enjoy!\n\n\nLinks:\n https://eres.fun/ \n https://eres.fun/invite \n https://eres.fun/support");
     } catch (error) {
       console.error("Error in the empty command:", error);
       message.channel.send("An error occurred. Please try again later.");
