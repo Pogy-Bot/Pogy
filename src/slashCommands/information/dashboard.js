7d8b702556adf9ef05497e482e718f8303419851
eYuM <101909226+eYuM-coder@users.noreply.github.com>
2024-03-23 12:37:04 -0600

    a

--- src/slashCommands/information/dashboard.js
+++ src/slashCommands/information/dashboard.js
@@ -4,11 +4,11 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
   .setName("dashboard")
-  .setDescription("Need help getting to the dashboard of Pogy? Use this command!"),
+  .setDescription("Need help getting to the dashboard of MEE8? Use this command!"),
   async execute(interaction) {
     const dashembed = new MessageEmbed()
-    .setTitle("Need Pogy's dashboard link?")
-    .setDescription("Click [here](https://v2.pogy.xyz) to see Pogy's dashboard")
+    .setTitle("Need MEE8's dashboard link?")
+    .setDescription("Click [here](https://g5qzg2-5003.csb.app) to see MEE8's dashboard")
     .setColor("RANDOM")
     .setFooter({ text: `Requested by ${interaction.author}` })
     .setTimestamp();
